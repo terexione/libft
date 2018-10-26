cbd55b46ae9b39ee13083b2cc1e064366d02dba2
Alibek Sansyzbayev <asansyzb@e1z2r4p12.42.us.org>
2018-10-25 17:50:16 -0700

    ez pz

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,23 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asansyzb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/23 15:30:53 by asansyzb          #+#    #+#             */
-/*   Updated: 2018/10/23 15:31:22 by asansyzb         ###   ########.fr       */
+/*   Created: 2018/10/23 15:55:08 by asansyzb          #+#    #+#             */
+/*   Updated: 2018/10/23 15:55:13 by asansyzb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnew(size_t size)
+int	ft_tolower(int c)
 {
-	char	*dest;
-
-	if (!(dest = (char*)malloc(sizeof(char) * size + 1)))
-		return ((void*)0);
-	ft_bzero(dest, size + 1);
-	return (dest);
+	if (c >= 'A' && c <= 'Z')
+		c = c + 32;
+	return (c);
 }
